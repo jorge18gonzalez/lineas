43e7f153c992c707d8d424249c60a67c8043f342
jorge andres gonzalez <jorgetgonzalez3@gmail.com>
2020-11-12 18:23:35 -0500

    se agregan las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import{MovilComponent}from './movil/movil.component';
+import{ MovilComponent } from './movil/movil.component';
+import{ QuienesComponent} from './quienes/quienes.component';
+import{StockComponent} from './stock/stock.component';
+
+
 
 const routes: Routes = [
-
+  {path: '' , component: StockComponent},
+  {}
 ];
 
 @NgModule({
