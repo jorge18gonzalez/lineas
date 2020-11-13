0ef87f5cec5230598a4f219eea35e1ec4bb47621
jorge andres gonzalez <jorgetgonzalez3@gmail.com>
2020-11-13 15:10:58 -0500

    se modifica el archivo model

--- src/app/telefonos.model.ts
+++ src/app/telefonos.model.ts
@@ -1,9 +1,10 @@
 export const TELEFONOS = [
     {"id": 1 , "marca": "xiomi" , "urlImg": "https://skymotion.com.co/wp-content/uploads/2020/04/redmi-note-8b.jpg " ,
-    "carateristicas":"Batería de 4000 mAh con carga rápida.Memoria de 64GB para guardar todos tus recuerdos.Pantalla de 6,3"},
+    "carateristicas": "Batería de 4000 mAh con carga rápida.Memoria de 64GB para guardar todos tus recuerdos.Pantalla de 6,3"},
 
     {"id" : 2 , "marca": "samsum" , "urlImg": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.samsung.com%2Fis%2Fimage%2Fsamsung%2Fco-galaxy-a30-a305-sm-a305gzkjcoo-frontblack-157746175%3F%24720_576_PNG%24&imgrefurl=https%3A%2F%2Fwww.samsung.com%2Fco%2Fsmartphones%2Fgalaxy-a%2Fgalaxy-a30-black-64gb-sm-a305gzkjcoo%2F&tbnid=Qed2u-YHFoPG2M&vet=12ahUKEwimhrOYn4DtAhVVGlkKHatAAz8QMygAegUIARC3AQ..i&docid=KiXNXYCHK0xNsM&w=720&h=576&q=samsung%20a30&ved=2ahUKEwimhrOYn4DtAhVVGlkKHatAAz8QMygAegUIARC3AQ",
-     "carateristicas":"Con tu Galaxy A30 no pierdas nada de tu contenido multimedia gracias a su gran pantalla de 6.4Infinity-UCaptura tus mejores momentos con tu con su camara Dual Con su carga rapida podrás disfrutar más!Sensor de desbloqueo de huella"},
+     "carateristicas": "Con tu Galaxy A30 no pierdas nada de tu contenido multimedia gracias a su gran pantalla de 6.4Infinity-UCaptura tus mejores momentos con tu con su camara Dual Con su carga rapida podrás disfrutar más!Sensor de desbloqueo de huella"},
      
-     {"id" : 3 ,"marca": "iphone" , "urlImg":""}
+     {"id" : 3 ,"marca": "morola" , "urlImg":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.celularesimportados.com.co%2Fwp-content%2Fuploads%2F2020%2F02%2Fcelular-motorola-moto-g8-plus-ram-4gb-64gb-1-ano-de-garantia-D_NQ_NP_823528-MCO32985867229_112019-F.jpg&imgrefurl=https%3A%2F%2Fwww.celularesimportados.com.co%2Fproducto%2Fmotorola-g8-plus-4ram-64-memoria%2F&tbnid=n3KUbT4NvgqKGM&vet=12ahUKEwjcje65qYDtAhUX3VkKHVupArcQMygHegUIARC9AQ..i&docid=GkmoyG4riFZV_M&w=710&h=676&q=motorola%20g8%20plus&ved=2ahUKEwjcje65qYDtAhUX3VkKHVupArcQMygHegUIARC9AQ",
+     "carateristicas": "Sensor 48 MPX + Night Vision.Cámara frontal 25 MPX.Más de un día de batería + Cargador Turbo Power.Pantalla Max Vision 6.3"}
 ]
