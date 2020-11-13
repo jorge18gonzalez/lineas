b374b612e4f756c58c32be17fa834f6a4189f406
jorge andres gonzalez <jorgetgonzalez3@gmail.com>
2020-11-13 14:52:13 -0500

    se crea el diccionario de datos

---
+++ src/app/telefonos.model.ts
@@ -0,0 +1,9 @@
+export const TELEFONOS = [
+    {"id": 1 , "marca": "xiomi" , "urlImg": "https://skymotion.com.co/wp-content/uploads/2020/04/redmi-note-8b.jpg " ,
+    "carateristicas":"Batería de 4000 mAh con carga rápida.Memoria de 64GB para guardar todos tus recuerdos.Pantalla de 6,3"},
+
+    {"id" : 2 , "marca": "samsum" , "urlImg": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.samsung.com%2Fis%2Fimage%2Fsamsung%2Fco-galaxy-a30-a305-sm-a305gzkjcoo-frontblack-157746175%3F%24720_576_PNG%24&imgrefurl=https%3A%2F%2Fwww.samsung.com%2Fco%2Fsmartphones%2Fgalaxy-a%2Fgalaxy-a30-black-64gb-sm-a305gzkjcoo%2F&tbnid=Qed2u-YHFoPG2M&vet=12ahUKEwimhrOYn4DtAhVVGlkKHatAAz8QMygAegUIARC3AQ..i&docid=KiXNXYCHK0xNsM&w=720&h=576&q=samsung%20a30&ved=2ahUKEwimhrOYn4DtAhVVGlkKHatAAz8QMygAegUIARC3AQ",
+     "carateristicas":"Con tu Galaxy A30 no pierdas nada de tu contenido multimedia gracias a su gran pantalla de 6.4Infinity-UCaptura tus mejores momentos con tu con su camara Dual Con su carga rapida podrás disfrutar más!Sensor de desbloqueo de huella"},
+     
+     {"id" : 3 ,"marca": "iphone" , "urlImg":""}
+]
