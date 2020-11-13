45891c57db86ee502a200a4f5b78c6751bf12fdf
jorge andres gonzalez <jorgetgonzalez3@gmail.com>
2020-11-12 19:19:00 -0500

    se creana las vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{ MovilComponent } from './movil/movil.component';
 import{ QuienesComponent} from './quienes/quienes.component';
-import{StockComponent} from './stock/stock.component';
+import{ StockComponent } from './stock/stock.component';
 
 
 
 const routes: Routes = [
   {path: '' , component: StockComponent},
-  {}
+  {path: 'equipo/:id' , component:MovilComponent},
+  {path: 'quienes' , component: QuienesComponent}
 ];
 
 @NgModule({
